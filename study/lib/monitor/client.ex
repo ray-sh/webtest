f73814d6549c310d721acd8e0ed518759304aeec
Lei Ding <leiding@leiding-a01.vmware.com>
2019-10-08 08:33:55 +0800

    Add code for raw socket server

--- study/lib/monitor/client.ex
+++ study/lib/monitor/client.ex
@@ -1,6 +1,6 @@
 defmodule DataClient do
    def connect() do
-     {:ok, socket} = :gen_tcp.connect('localhost', 5000, [:binary])
+     {:ok, socket} = :gen_tcp.connect('localhost', 5001, [:binary])
      {:ok, client} = Task.start(__MODULE__, :send_info, [socket,1])
      client
    end
@@ -17,7 +17,7 @@ defmodule DataClient do
     #如果没有“\n”,数据能发过去吗？server那里如何知道数据发送完成？他是通过EOF来识别
     #如果最后一个字符不是\n,send数据能发过去吗？
     #"\n" = <<10>>
-    :gen_tcp.send(socket, <<0x0b>> <> "The temperature is #{temp}" <> <<0x1c>> <> <<11>>)
+    :gen_tcp.send(socket, <<0x0b>> <> "The temperature is #{temp}" <> <<0x1c, 0x0d, 10>> )
      #timer.sleep的单位时毫秒
      :timer.sleep(5000)
      send_info(socket, temp + 1)  
